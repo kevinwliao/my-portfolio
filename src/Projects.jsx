8cd866641c20c2b3ecebbe8ef465892d6fb7285e
Kevin Liao <kliao14@gmail.com>
2023-10-12 17:57:18 -0700

    created constants for links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,12 +6,24 @@ import bub from "./assets/bub.jpeg";
 import shep from "./assets/shep.jpeg";
 
 const CHUCK_LINK = "https://chuck.stanford.edu/";
+const NIGHTMARE_LIBRARY_CODE_LINK =
+  "https://github.com/kevinwliao/nightmare-library/blob/main/nightmare-library.ck";
 const WEBCHUCK_LINK = "https://chuck.cs.princeton.edu/webchuck/";
 const FAUST_LINK = "https://faust.grame.fr/";
 const BUB_LINK = "https://ccrma.stanford.edu/~kliao14/bub/";
 const SHEP_LINK = "https://ccrma.stanford.edu/~kliao14/shep/";
-const GLASS_ETUDE_SCORE_LINK =
+const GLASS_PRELUDE_SCORE_LINK =
   "https://musescore.com/user/61065196/scores/12555049";
+
+const RUNNING_LINK =
+  "https%3A//api.soundcloud.com/tracks/1637020254%3Fsecret_token%3Ds-UWMleiZdvYz&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
+const GLASS_PRELUDE_LINK =
+  "https://soundcloud.com/kevin-liao-775540098/glass-etude?si=160f620fade249149439492bb6944e6b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+const NIGHTMARE_LIBRARY_LINK =
+  "https://soundcloud.com/kevin-liao-775540098/nightmare-library?si=74adf6351ffd453e86d08d3f58ffa0bc&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+const SKY_BURIAL_LINK =
+  "https://soundcloud.com/kevin-liao-775540098/sky-burial?in=kevin-liao-775540098/sets/sky-burial&si=32033cdb508f49678b2338626cdf31aa&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+const BEANDOLIN_LINK = "https://youtu.be/H8oFNkZPV88";
 
 const Projects = () => {
   return (
@@ -26,7 +38,7 @@ const Projects = () => {
               Saxophone Quartet, Stanford CCRMA 2022
             </h3>
           </div>
-          <Player url="https%3A//api.soundcloud.com/tracks/1637020254%3Fsecret_token%3Ds-UWMleiZdvYz&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
+          <Player url={RUNNING_LINK} />
 
           <p className="mb-2">
             "Running, Stomping In My Sleep" is a piece for saxophone quartet.
@@ -50,19 +62,19 @@ const Projects = () => {
 
         <div className="mb-6" id="piece">
           <div className="pb-5">
-            <h2 className="text-3xl font-extrabold">Glass Étude</h2>
+            <h2 className="text-3xl font-extrabold">Glass Prelude</h2>
             <h3 className="text-xl font-light text-slate-500">Piano, 2022</h3>
           </div>
-          <Player url="https://soundcloud.com/kevin-liao-775540098/glass-etude?si=8e201fa2a11f4b1d9701e0578116878c&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" />
+          <Player url={GLASS_PRELUDE_LINK} />
 
           <p className="mb-2">
-            "Glass Étude" is a study concerning the construction of rhythmic
+            "Glass Prelude" is a study concerning the construction of rhythmic
             motifs.
           </p>
           <p>
             {" "}
             <a
-              href={GLASS_ETUDE_SCORE_LINK}
+              href={GLASS_PRELUDE_SCORE_LINK}
               className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
               target="_blank"
               rel="noopener noreferrer"
@@ -81,7 +93,7 @@ const Projects = () => {
             <h2 className="text-3xl font-extrabold">Nightmare Library</h2>
             <h3 className="text-xl font-light text-slate-500">2022</h3>
           </div>
-          <Player url="https://soundcloud.com/kevin-liao-775540098/nightmare-library?si=74adf6351ffd453e86d08d3f58ffa0bc&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" />
+          <Player url={NIGHTMARE_LIBRARY_LINK} />
           <p className="mb-2">
             "Nightmare Library" is inspired by the work of Paul Lansky, meant to
             evoke the feeling of being overwhelmed by noise in a library. This
@@ -104,7 +116,7 @@ const Projects = () => {
             If you would like to run the ChucK{" "}
             <a
               className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-              href={CHUCK_LINK}
+              href={NIGHTMARE_LIBRARY_CODE_LINK}
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -119,7 +131,7 @@ const Projects = () => {
             <h2 className="text-3xl font-extrabold">Sky Burial</h2>
             <h3 className="text-xl font-light text-slate-500">2022</h3>
           </div>
-          <Player url="https://soundcloud.com/kevin-liao-775540098/sky-burial?in=kevin-liao-775540098/sets/sky-burial&si=32033cdb508f49678b2338626cdf31aa&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" />
+          <Player url={SKY_BURIAL_LINK} />
           <p className="">
             "Sky Burial" refers to a Tibetan Buddhist practice in which a body
             is generously fed to Himalayan vultures. This EP explores sound
@@ -168,7 +180,7 @@ const Projects = () => {
             <h2 className="text-3xl font-extrabold">Beandolin</h2>
             <h3 className="text-xl font-light text-slate-500">2023</h3>
           </div>
-          <Player url="https://youtu.be/H8oFNkZPV88"></Player>
+          <Player url={BEANDOLIN_LINK}></Player>
           <p className="mb-2">
             Beandolin is a musical instrument that uses a Teensy microcontroller
             to produce sound. A piezo microphone attached to the bottom of the
